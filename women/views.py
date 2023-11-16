9528c83a9ad9fbb2f3d51653862872a8c97fee63
tim2004timi <tim2004timi@mail.ru>
2023-11-16 13:01:09 +0300

    Update DRF 3th lesson

--- women/views.py
+++ women/views.py
@@ -1,10 +1,27 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from women.models import Women
 from women.serializers import WomenSerializer
 
 
-class WomenAPIView(generics.ListAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+class WomenAPIView(APIView):
+    def get(self, request):
+        women = Women.objects.all().values()
+        return Response({'posts': list(women)})
+
+    def post(self, request):
+        new_post = Women.objects.create(
+            title=request.data["title"],
+            content=request.data["content"],
+            cat_id=request.data["cat_id"]
+        )
+
+        return Response({"post": model_to_dict(new_post)})
+
+# class WomenAPIView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
